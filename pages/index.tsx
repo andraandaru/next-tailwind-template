06191896661f6e53cda7c410114e25ef4a4f45b0
Andra Andaru <andraandaruu@gmail.com>
2022-01-27 15:13:43 +0700

    add alliases, create card component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Card from '@/components/Card'
 import Head from 'next/head'
 
 export default function Home() {
@@ -24,45 +25,29 @@ export default function Home() {
         </p>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
+          <Card
             href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+            title="Documentation"
+            desc="Find in-depth information about Next.js features and API."
+          />
 
-          <a
+          <Card
             href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+            title="Learn"
+            desc="Learn about Next.js in an interactive course with quizzes!"
+          />
 
-          <a
+          <Card
             href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+            title="Examples"
+            desc="Discover and deploy boilerplate example Next.js projects."
+          />
 
-          <a
+          <Card
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            title="Deploy"
+            desc="Instantly deploy your Next.js site to a public URL with Vercel."
+          />
         </div>
       </main>
 
